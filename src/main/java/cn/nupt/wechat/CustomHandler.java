667ce91a17259b7a561f8dcf93a24ad3111c7176
pingan <pizan@foxmail.com>
2019-05-28 16:12:27 +0800

    修改

--- src/main/java/cn/nupt/wechat/CustomHandler.java
+++ src/main/java/cn/nupt/wechat/CustomHandler.java
@@ -18,7 +18,7 @@ import io.netty.util.CharsetUtil;
 //SimpleChannelInboundHandler:对于请求来讲，相当于"入站，入境"
 public class CustomHandler extends SimpleChannelInboundHandler<HttpObject> {
 
-
+    
     @Override
     protected void channelRead0(ChannelHandlerContext chx
             , HttpObject msg) throws Exception {
@@ -47,5 +47,66 @@ public class CustomHandler extends SimpleChannelInboundHandler<HttpObject> {
         }
 
 
+    }
+
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("注册：" + 1);
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("移除：" + 1);
+        super.channelUnregistered(ctx);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("活跃：" + 1);
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("不活跃：" + 1);
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("读取完毕：" +1 );
+        super.channelReadComplete(ctx);
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        System.out.println("用户时间触发：" + 1);
+        super.userEventTriggered(ctx, evt);
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel可写更改：" + 1);
+        super.channelWritabilityChanged(ctx);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("捕获到异常：" + 1);
+        super.exceptionCaught(ctx, cause);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("助手类添加：" + 1);
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("助手类移除：" + 1);
+        super.handlerRemoved(ctx);
     }
 }
